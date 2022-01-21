7b1a03a2766b43ecd437f6a4818b2126a7ffb3ee
1635334137 <18877888233@163.com>
2022-01-21 19:56:30 +0800

    修复聚合项目使用@Data注解无法获取set/get（建议不使用@Data）

--- shop_service_common/src/main/java/com/shop/entity/Product.java
+++ shop_service_common/src/main/java/com/shop/entity/Product.java
@@ -11,7 +11,6 @@ import java.math.BigDecimal;
  * @author Mr.Lan
  * @create: 2022-01-21 14:25
  */
-@Data
 @Entity
 @Table(name = "tb_product")
 public class Product {
@@ -30,4 +29,51 @@ public class Product {
 
     private String caption;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getProductDesc() {
+        return productDesc;
+    }
+
+    public void setProductDesc(String productDesc) {
+        this.productDesc = productDesc;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
 }
